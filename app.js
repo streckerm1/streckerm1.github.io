27c0ab5c5c4d11f584a8cf54889523e1cf91ab72
streckerm1 <strecker.matt4@gmail.com>
2017-08-01 15:35:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 2561,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'Untitled Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6Lf14A8UAAAAAIJiOJTf4f1feQbxuCmMZVdjBVOY',  // <----- EDIT ME!
+  redirect_uri: 'https://streckerm1.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
